833953bb49e7510b6bc55f88484ab5a7dff3408d
Gianmarco Guizzardi <guizzardi.gianmarco@panettipitagora.edu.it>
2022-11-12 17:33:59 +0100

    test : inizio del js

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+var hp = document.querySelector('.vita');
+var attack = document.querySelector('.attacco');
+var defense = document.querySelector('.difesa');
+var speed = document.querySelector('.velocita');
+var specialAttack = document.querySelector('.attacco_spaziale');
+var specialDefense = document.querySelector('.difesa_speciale');
+var button = document.querySelector('.bottne');
+
+button.addEventListener('click', function() {
+    if (checkRequired(hp.value) && checkValue(hp.value) && checkRequired(attack.value) && checkValue(attack.value) && checkRequired(defense.value) && checkValue(defense.value) && checkRequired(speed.value) && checkValue(speed.value) && checkRequired(specialAttack.value) && checkValue(specialAttack.value) && checkRequired(specialDefense.value) && checkValue(specialDefense.value)) {
+        alert('ok');
+    } else {
+        alert('errore');
+    }
+ });
+
+function checkRequired(value) {
+    if (value.trim() === '') {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+function checkValue(value) {
+    if (value < 0 || value > 255) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+
