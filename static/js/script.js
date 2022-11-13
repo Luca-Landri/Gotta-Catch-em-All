0795fa2c07556180a6d913cfd85ea14ad7d22a15
Gianmarco Guizzardi <guizzardi.gianmarco@panettipitagora.edu.it>
2022-11-13 10:45:29 +0100

    feat: cambiata variabile da var a let

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
-var hp = document.querySelector('.vita');
-var attack = document.querySelector('.attacco');
-var defense = document.querySelector('.difesa');
-var speed = document.querySelector('.velocita');
-var specialAttack = document.querySelector('.attacco_spaziale');
-var specialDefense = document.querySelector('.difesa_speciale');
-var button = document.querySelector('.bottne');
+let hp = document.querySelector('.vita');
+let attack = document.querySelector('.attacco');
+let defense = document.querySelector('.difesa');
+let speed = document.querySelector('.velocita');
+let specialAttack = document.querySelector('.attacco_spaziale');
+let specialDefense = document.querySelector('.difesa_speciale');
+let button = document.querySelector('.bottone');
 
 button.addEventListener('click', function() {
     if (checkRequired(hp.value) && checkValue(hp.value) && checkRequired(attack.value) && checkValue(attack.value) && checkRequired(defense.value) && checkValue(defense.value) && checkRequired(speed.value) && checkValue(speed.value) && checkRequired(specialAttack.value) && checkValue(specialAttack.value) && checkRequired(specialDefense.value) && checkValue(specialDefense.value)) {
@@ -29,5 +29,3 @@ function checkValue(value) {
         return true;
     }
 };
-
-
