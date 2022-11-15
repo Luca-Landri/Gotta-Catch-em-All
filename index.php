abbd2916480c8b1c92c14065e73cf9513e3a7013
ludovicadeliaa <92517081+ludovicadeliaa@users.noreply.github.com>
2022-11-15 21:43:44 +0100

    #3388y6v
    Link of the front-end and the back-end

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 header("Location: ../skels/index.html");
+
+$HP = $_POST['HP'];
+$ATK = $_POST['ATK'];
+$DEF = $_POST['DEF'];
+$SPTK = $_POST['SPTK'];
+$SPDEF = $_POST['SPDEF'];
+$SPEED = $_POST['SPEED'];
+
+if(isset( $_POST['HP']) && isset( $_POST['ATK'])  && isset( $_POST['DEF'])  && isset( $_POST['SPTK'])  && isset( $_POST['SPDEF'])  && isset( $_POST['SPEED'])){
+
+}else{
+    echo "I campi non sono stati compilati correttamente";
+}
 ?>
