b730ab2912cf305b7fc3da6ddef37ca953e9d2ab
ludovicadeliaa <92517081+ludovicadeliaa@users.noreply.github.com>
2022-11-23 20:36:44 +0100

    fixata lettura csv

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $SPDEF = $_POST['difesapeciale'];
 $SPEED = $_POST['velocita'];
 
 function read_csv() {
-    $file = fopen("../data/pokemon.csv", "r");
+    $file = fopen("data/pokemon.csv", "r");
     $data = array();
     while (($line = fgetcsv($file)) !== FALSE) {
         $data[] = $line;
@@ -16,12 +16,13 @@ function read_csv() {
     return $data;
 }
 
-if(isset( $_POST['HP']) && isset( $_POST['ATK'])  && isset( $_POST['DEF'])  && isset( $_POST['SPTK'])  && isset( $_POST['SPDEF'])  && isset( $_POST['SPEED'])) {
+
+
+if(isset( $_POST['vita']) && isset( $_POST['attacco'])  && isset( $_POST['difesa'])  && isset( $_POST['attspeciale'])  && isset( $_POST['difesapeciale'])  && isset( $_POST['velocita'])) {
 
     if(is_numeric($HP) && is_numeric($ATK) && is_numeric($DEF) && is_numeric($SPTK) && is_numeric($SPDEF) && is_numeric($SPEED)) {
         if($HP > 0 && $HP < 300 && $ATK > 0 && $ATK < 300 && $DEF > 0 && $DEF < 300 && $SPTK > 0 && $SPTK < 300 && $SPDEF > 0 && $SPDEF < 300 && $SPEED > 0 && $SPEED < 300){
             $data = read_csv();
-            $data[] = array($HP, $ATK, $DEF, $SPTK, $SPDEF, $SPEED);
             for ($i = 1; $i < count($data); $i++) {
                 echo "<br>";
                 for ($j = 0; $j < count($data[$i]); $j++) {
